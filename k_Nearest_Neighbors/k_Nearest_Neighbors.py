cc56f429d21839dd4e33b13f836f8db62fdf5d7a
Tony Wang <563726573@qq.com>
2017-06-16 14:15:24 +0800

    Addition of Condensend Nearest Neighbors algorithm

--- k_Nearest_Neighbors/k_Nearest_Neighbors.py
+++ k_Nearest_Neighbors/k_Nearest_Neighbors.py
@@ -1,17 +1,14 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import sys
-sys.path.append('D:\Machine Learning\Machine-Learning\Random_Data_Generator')
-import RandGen
 
 
 class kNearestNeighbors():
     def __init__(self,dataList,display=False):
         self.dataList = dataList
         self.InputPoint = None
-        self.a,self.b,self.c,self.d = self.SplitDataList()
         self.display = display
+        self.a,self.b,self.c,self.d = self.SplitDataList()
         self.dataListCopy = dataList
 
     def GetDistance(self,a,b):
@@ -21,7 +18,7 @@ class kNearestNeighbors():
         :return: the distance between a and b
         """
         distance = 0
-        for i in range(0,len(a),1):
+        for i in range(0,len(a)-1,1):
             distance = distance + (a[i] - b[i])**2
         return math.sqrt(float(distance))
 
@@ -53,7 +50,6 @@ class kNearestNeighbors():
         :param InputPoint:
         :return: The index of the nearest neighbor of InputPoint in self.dataList
         """
-        self.dataList = self.dataListCopy
         place = 0
         shortest = self.GetDistance(InputPoint,self.dataList[0])
         for i in range(0,len(self.dataList),1):
@@ -63,13 +59,67 @@ class kNearestNeighbors():
                 place = i
         return place
 
-    def kNearestNeighbors(self,k,InputPoint):
+    def GetConsistency(self,k,TrainingSet):
+        """
+        :param k: int, number of neighbors
+        :param TrainingSet: matrix, condensed input set
+        :return: the index of the first point in the original input set that results in a different classification
+        if classified by the condensed input set
+        """
+        TrainingKNN = kNearestNeighbors(TrainingSet)
+        for i in range(0,len(self.dataList),1):
+            if TrainingKNN.KNearestNeighbors(k,self.dataList[i]) != self.KNearestNeighbors(k,self.dataList[i]):
+                return -1,i
+        return 1,i
+
+    def GetMisClassified(self,k,Center):
+        """
+        :param k: int, number of neighbors
+        :param Center: a 1x2 array, the exact point found by GetConsistency that has a different classification
+        by the original and the condensed input set
+        :return:
+        """
+        x = Center[0] + 2 * (np.random.rand() - 0.5)
+        y = Center[1] + 2 * (np.random.rand() - 0.5)
+        Center[0] = x
+        Center[1] = y
+        return Center
+
+    def FindElement(self,A,B):
+        """
+        :param A: an element
+        :param B: a set
+        :return: boolean, whether A is an element of B
+        """
+        for i in B:
+            if np.array_equal(i,A):
+                return True
+        return False
+
+    def DataCondensing(self,k):
+        TrainingSet = self.dataList[0:k,:]
+        while self.GetConsistency(k,TrainingSet)[0] == -1:
+            self.dataList = self.dataListCopy
+            X_star = self.GetMisClassified(k,self.dataList[self.GetConsistency(k,TrainingSet)[1]])
+            print X_star
+            while self.FindElement(self.dataList[self.FindNearestNeighbor(X_star)], TrainingSet) \
+                    or kNearestNeighbors(self.dataList).KNearestNeighbors(k,self.dataList[self.FindNearestNeighbor(X_star)]) != \
+                            kNearestNeighbors(self.dataList).KNearestNeighbors(k,X_star):
+                self.dataList = np.delete(self.dataList, self.FindNearestNeighbor(X_star), 0)
+            TrainingSet = np.concatenate((TrainingSet, [self.dataList[self.FindNearestNeighbor(X_star)]]),axis=0)
+            print "Condensed Set Size: ", len(TrainingSet)
+        self.dataList = self.dataListCopy = TrainingSet
+
+    def KNearestNeighbors(self,k,InputPoint,DataCondensing=False):
         """
         :param k: number of nearest neighbors
         :param InputPoint:
         :return: The classification of InputPoint based on its k nearest neighbors
         """
+        if DataCondensing:
+            self.DataCondensing(k)
         self.InputPoint = InputPoint
+        self.dataList = self.dataListCopy
         Neighbors = []
         for i in range(0,k,1):
             NearestNeighborIndex = self.FindNearestNeighbor(InputPoint)
@@ -81,11 +131,13 @@ class kNearestNeighbors():
                 NumberOfPositive += 1
         if NumberOfPositive > (len(Neighbors)-1)/2:
             self.Classification = 1
+            self.dataList = self.dataListCopy
             if self.display:
                 plt.plot(self.InputPoint[0], self.InputPoint[1], "go")
             return 1
         else:
             self.Classification = -1
+            self.dataList = self.dataListCopy
             if self.display:
                 plt.plot(self.InputPoint[0], self.InputPoint[1], "yo")
             return -1
@@ -106,5 +158,4 @@ class kNearestNeighbors():
         yMax = max(max(b),max(d))+1
         plt.axis((xMin,xMax,yMin,yMax))
         plt.show()
-
 
--- k_Nearest_Neighbors/k_Nearest_Neighbors_test.py
+++ k_Nearest_Neighbors/k_Nearest_Neighbors_test.py
@@ -6,10 +6,11 @@ import RandGen
 
 
 Generator = RandGen.RandomDataGenerator()
-DataList = Generator.GenerateBinaryData(100,[1,-2,-3,1],Poly=True)
+DataList = Generator.GenerateBinaryData(500,[1,-2,-3,1],Poly=True,noise=0)
 test = k_Nearest_Neighbors.kNearestNeighbors(DataList,display=True)
-testData = numpy.random.rand(100,2)
+testData = numpy.random.rand(1000,2)
 testData = testData * 10
+test.DataCondensing(6)
 for i in testData:
-    test.kNearestNeighbors(3,i)
+    test.KNearestNeighbors(6,i)
 test.Visualization()
