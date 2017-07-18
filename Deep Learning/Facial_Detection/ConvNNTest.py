32d19737b94eec72bab54f171cad472f43c4839a
Tony Wang <563726573@qq.com>
2017-07-18 10:19:04 +0800

    Commit

--- Deep Learning/Facial_Detection/ConvNNTest.py
+++ Deep Learning/Facial_Detection/ConvNNTest.py
@@ -13,8 +13,16 @@ with open(pickle_file, 'rb') as f:
   valid_labels = save['valid_labels']
   del save  # hint to help gc free up memory
 
+def error(predictions, labels):
+    return np.sum(np.power(predictions - labels, 2)) / predictions.shape[0]
+def accuracy(predictions, labels):
+    count = 0
+    for i in range(len(predictions)):
+        if error(predictions[i],labels[i]) < 10:
+            count += 1
+    return float(count)/len(predictions)*100
 
-model = 'CNN(50,5x3x3x3,36,512x512,60000)'
+model = 'CNN(50,5x3x3x3,36,512x512,50000)'
 session = tf.Session()
 saver = tf.train.import_meta_graph('C:\\Users\\alien\Desktop\Deep_Learning_Data\model\\ConvolutionalNeuralNetworksOnFacialDetection\\{}\Saved.meta'.format(model))
 saver.restore(session,'C:\\Users\\alien\Desktop\Deep_Learning_Data\model\\ConvolutionalNeuralNetworksOnFacialDetection\\{}\Saved'.format(model))
@@ -27,19 +35,22 @@ keep_prob = graph.get_tensor_by_name('keep_prob:0')
 dataset = test_dataset
 labels = test_labels
 
-with open('C:\\Users\\alien\Desktop\Deep_Learning_Data\\Data\\zheyew(143x192).pickle','rb') as f:
-    save = pickle.load(f)
-    dataset = save['picture']
-    dataset = np.reshape(dataset,(1,143,192))
+# with open('C:\\Users\\alien\Desktop\Deep_Learning_Data\\Data\\zheyew(143x192).pickle','rb') as f:
+#     save = pickle.load(f)
+#     dataset = save['picture']
+#     dataset = np.reshape(dataset,(1,143,192))
 
 print(dataset.shape)
-for i in range(1):
+prediction_list = []
+for i in range(200):
     test_input = np.reshape(dataset[i],(1,dataset[0].shape[0],dataset[0].shape[1],1))
-    test_output = np.reshape(labels[i],(1,labels[0].shape[0]))
+    # test_output = np.reshape(labels[i],(1,labels[0].shape[0]))
     output = session.run(test_prediction,feed_dict={tf_test_dataset:test_input, keep_prob:1.0})
-    plt.imshow(dataset[i],cmap='gray')
-    plt.plot([output[0][0]],[output[0][1]],'rx')
-    plt.plot([output[0][2]],[output[0][3]],'rx')
-#    plt.plot([labels[i][0]],[labels[i][1]],'gx')
-#    plt.plot([labels[i][2]],[labels[i][3]],'gx')
-    plt.show()
+    prediction_list.append(output)
+    # plt.imshow(dataset[i],cmap='gray')
+    # plt.plot([output[0][0]],[output[0][1]],'rx')
+    # plt.plot([output[0][2]],[output[0][3]],'rx')
+    # plt.plot([labels[i][0]],[labels[i][1]],'gx')
+    # plt.plot([labels[i][2]],[labels[i][3]],'gx')
+    # plt.show()
+print(accuracy(prediction_list,labels))
