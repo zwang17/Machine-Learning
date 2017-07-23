8cd40a8304aede307df4dfca2ad49158fc72664f
Tony Wang <563726573@qq.com>
2017-07-23 16:17:59 +0800

    Commit

--- Deep Learning/Facial_Detection(Kaggle)/Try(Facial_Detection(Kaggle)).py
+++ Deep Learning/Facial_Detection(Kaggle)/Try(Facial_Detection(Kaggle)).py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
 import numpy as np
 import sys
 import pandas as pd
@@ -6,6 +6,7 @@ from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 from six.moves import cPickle as pickle
 
+print_tensors_in_checkpoint_file(tensor_name=None,file_name='C:\\Users\\alien\Desktop\Deep_Learning_Data\Model\Facial Detection\CNN(50,5x3x3x3,36,512x512,50000)',all_tensors=True)
 
 def read_pgm(pgmf):
     """Return a raster of integers from a PGM as a list of lists."""
